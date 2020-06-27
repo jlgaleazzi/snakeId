8ba923e2620e23036d351b093ba4519f1c0d9ab5
Juan Galeazzi <jgaleazzi@gmail.com>
2020-06-27 15:48:50 -0500

    working snake id

---
+++ front-end/src/components/common/loadingDits.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LoadingDots = () => {
+    return (
+        <div class="spinner-border m-5" role="status">
+            <span class="sr-only">Analizing...</span>
+        </div>
+    )
+
+}
+
+export default LoadingDots;
--- front-end/src/components/picLoader.jsx
+++ front-end/src/components/picLoader.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import {Button, Card} from 'react-bootstrap'
+import { Button, Card } from 'react-bootstrap'
 import snake from '../assets/snake.png'
-import venomous from '../assets/venomous.png'
+import LoadingDots from './common/loadingDits';
 
 
 
 const PicLoader = props => {
-    const {data, dispatch} = props;
-    
+    const { data, dispatch } = props;
+
 
     const handleDragEnter = e => {
         e.preventDefault();
         e.stopPropagation();
-        dispatch({type:'SET_DROP_DEPTH', dropDepth: data.dropDepth + 1})
+        dispatch({ type: 'SET_DROP_DEPTH', dropDepth: data.dropDepth + 1 })
     }
 
     const handleDragLeave = e => {
         e.preventDefault();
         e.stopPropagation();
-        dispatch({type:'SET_DROP_DEPTH',dropDepth: data.dropDepth - 1 });
+        dispatch({ type: 'SET_DROP_DEPTH', dropDepth: data.dropDepth - 1 });
         if (data.dropDepth > 0) return
-        dispatch({type: 'SET_IN_DROP_ZONE', inDropZone:false})
+        dispatch({ type: 'SET_IN_DROP_ZONE', inDropZone: false })
 
     }
 
@@ -28,7 +28,7 @@ const PicLoader = props => {
         e.preventDefault();
         e.stopPropagation();
         e.dataTransfer.dropEffect = 'copy';
-        dispatch({type: 'SET_IN_DROP_ZONE', inDropZone:true})
+        dispatch({ type: 'SET_IN_DROP_ZONE', inDropZone: true })
     }
 
     const handleDrop = e => {
@@ -38,30 +38,31 @@ const PicLoader = props => {
         let file = null;
         if (e.target.id === 'file-button') {
             file = e.target.files[0];
-        } else  {
+        } else {
             file = e.dataTransfer.files[0];
             e.dataTransfer.clearData();
         }
         if (file != null) {
             let fr = new FileReader();
-            fr.onload = function(event){
+            fr.onload = function (event) {
                 let img = event.target.result;
-                dispatch({type: 'FILE_SELECTED', image:img})
-                }
+                dispatch({ type: 'FILE_SELECTED', image: img })
+            }
             fr.readAsDataURL(file);
+            dispatch({ type: "ANALIZING", analizing: true })
             sendImageToServer(file)
                 .then(data => {
-                    console.log(data);
-                    dispatch({type:'GOT_RESULT', result:data})
+                    dispatch({ type: "ANALIZING", analizing: false })
+                    dispatch({ type: 'GOT_RESULT', result: data })
                 })
         }
-        dispatch({type: 'SET_DROP_DEPTH', dropDepth:0});
-        dispatch({type: 'SET_IN_DROP_ZONE', inDropZone:false })
+        dispatch({ type: 'SET_DROP_DEPTH', dropDepth: 0 });
+        dispatch({ type: 'SET_IN_DROP_ZONE', inDropZone: false })
     }
 
-    async function sendImageToServer(img){
+    async function sendImageToServer(img) {
         const form_data = new FormData();
-        form_data.append('file',img);
+        form_data.append('file', img);
         const url = '/idmysnake';
         const request_Obj = {
             method: 'POST',
@@ -70,66 +71,77 @@ const PicLoader = props => {
             headers: {
                 'accept': '*/*',
                 'Accept-Language': 'en-US,en;q=0.8',
-              },
+            },
         }
-       
+
         const response = await fetch(url, request_Obj);
         return response.json();
     }
-    
+
     const getProperties = () => {
-        
+
         if (data.result) {
             if (data.result.venomous === 'true') {
-                return {bg:'danger',label:'venomous'}
-            } 
-            if (data.result.venomous === 'undetermined') 
-            {
-                return {bg:'warning',label:''}
+                return { bg: 'danger', label: 'venomous' }
+            }
+            if (data.result.venomous === 'undetermined') {
+                return { bg: 'warning', label: '' }
             }
         }
-        return {bg:'light',label:'Non venomous'}
+        return { bg: 'light', label: 'Non venomous' }
     }
-    
+
 
 
     return (
-        
-    <div className="pic-container">
-       
-        <div 
-        onDrop = { e => handleDrop(e)}
-        onDragOver = { e => handleDragOver(e)}
-        onDragEnter = { e => handleDragEnter(e) }
-        onDragLeave = { e => handleDragLeave(e)} 
-        >
-        <Card bg={getProperties().bg} style={{width:'30rem', height:'auto'}}>
-            <Card.Body >
-                <Card.Img variant='top' className={'card-img-top'} src={data.image !== null ? data.image : snake}/>
-                <Card.Title>{data.result !== null ? data.result.snake : "Upload a Snake Pic"}</Card.Title>
-                <Card.Text>
-                   {data.result !== null ? getProperties().label : ''}
-                </Card.Text>
-                <div className='upload-btn-wrapper'>
-                <Button className='btn btn-primary btn-lg'>
-                    <svg className="bi bi-cloud-upload" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M4.887 6.2l-.964-.165A2.5 2.5 0 1 0 3.5 11H6v1H3.5a3.5 3.5 0 1 1 .59-6.95 5.002 5.002 0 1 1 9.804 1.98A2.501 2.501 0 0 1 13.5 12H10v-1h3.5a1.5 1.5 0 0 0 .237-2.981L12.7 7.854l.216-1.028a4 4 0 1 0-7.843-1.587l-.185.96z"/>
-                        <path fillRule="evenodd" d="M5 8.854a.5.5 0 0 0 .707 0L8 6.56l2.293 2.293A.5.5 0 1 0 11 8.146L8.354 5.5a.5.5 0 0 0-.708 0L5 8.146a.5.5 0 0 0 0 .708z"/>
-                        <path fillRule="evenodd" d="M8 6a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0v-8A.5.5 0 0 1 8 6z"/>
-                    </svg>   
-                    &nbsp;Upload snake
-                </Button>
-                <input type="file" className='input-btn' accept='image/*' id='file-button' onChange={e=> handleDrop(e)}/>
+
+        <div className="pic-container">
+
+            <div
+                onDrop={e => handleDrop(e)}
+                onDragOver={e => handleDragOver(e)}
+                onDragEnter={e => handleDragEnter(e)}
+                onDragLeave={e => handleDragLeave(e)}
+            >
+                <Card bg={getProperties().bg} style={{ width: '30rem', height: 'auto' }}>
+                    <Card.Body >
+                        <Card.Img variant='top' className={'card-img-top'} src={data.image !== null ? data.image : snake} />
+                        <Card.Text></Card.Text>
+                        <Card.Title>{
+                            data.analizing ? <p>Analizing Picture</p> : (data.result !== null ? data.result.snake : "Upload a Snake Pic")}
+                        </Card.Title>
+                        <Card.Text>
+                            {data.result !== null ? getProperties().label : <p>&nbsp;</p>}
+                        </Card.Text>
+                        <div className='upload-btn-wrapper'>
+                            <Button className='btn btn-primary' disabled={data.analizing}>
+                                {
+                                    data.analizing ?
+                                        <div>
+                                            <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                                            &nbsp;Analizing...
+                                    </div> :
+                                        <div>
+                                            <svg className="bi bi-cloud-upload" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M4.887 6.2l-.964-.165A2.5 2.5 0 1 0 3.5 11H6v1H3.5a3.5 3.5 0 1 1 .59-6.95 5.002 5.002 0 1 1 9.804 1.98A2.501 2.501 0 0 1 13.5 12H10v-1h3.5a1.5 1.5 0 0 0 .237-2.981L12.7 7.854l.216-1.028a4 4 0 1 0-7.843-1.587l-.185.96z" />
+                                                <path fillRule="evenodd" d="M5 8.854a.5.5 0 0 0 .707 0L8 6.56l2.293 2.293A.5.5 0 1 0 11 8.146L8.354 5.5a.5.5 0 0 0-.708 0L5 8.146a.5.5 0 0 0 0 .708z" />
+                                                <path fillRule="evenodd" d="M8 6a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0v-8A.5.5 0 0 1 8 6z" />
+                                            </svg>
+                                            <span>&nbsp;Upload Snake</span>
+                                        </div>
+                                }
+                            </Button>
+                            <input type="file" className='input-btn' accept='image/*' id='file-button' onChange={e => handleDrop(e)} />
+                        </div>
+                    </Card.Body>
+                </Card>
+
             </div>
-            </Card.Body>
-        </Card>
-        
+
+
+
         </div>
 
-
-      
-    </div>
-        
     )
 }
 

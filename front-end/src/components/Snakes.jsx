b7333e16a8509ab389c052da204c68175a17eb31
Juan Galeazzi <jgaleazzi@gmail.com>
2020-07-08 14:56:45 -0500

    routes working

--- front-end/src/components/Snakes.jsx
+++ front-end/src/components/Snakes.jsx
@@ -1,27 +1,20 @@
 import React from "react"
 import { Card } from 'react-bootstrap'
 
-const Snakes = () => {
+const Snakes = (props) => {
     return (
 
-        <Card>
+        < Card bg='light' style={{ width: '30rem' }}>
             <Card.Body>
-                <Card.Img variant="left" src="../assets/snake.png" />
-                <Card.Title>Coachwhip Snake</Card.Title>
-                <Card.Subtitle>(Masticophis flagellum)</Card.Subtitle>
-                <Card.Text>
-                    is a species of nonvenomous colubrid snake,
-                    commonly referred to as the coachwhip or the whip snake,
-                    which is endemic to the United States and Mexico. Coachwhips are diurnal,
-                    and actively hunt and eat lizards, small birds, and rodents.
-                    Coachwhips subdue prey by grasping and holding them with their jaws and do not use constriction.
-                    [6] They tend to be sensitive to potential threats,
-                    and often bolt at the first sign of one; they are extremely fast-moving snakes.
-                    They are curious snakes with good eyesight, and are sometimes seen raising their heads above the level of the
-                    grass or rocks to see what is around them. They can slither up to 15 mph.
+                <Card.Img variant="top" src={props.data.image} />
+                <Card.Title>{props.data.longName}</Card.Title>
+                <Card.Subtitle>({props.data.sciName})</Card.Subtitle>
+                <Card.Text style={{ textAlign: 'left' }}>
+                    {props.data.description}
                 </Card.Text>
             </Card.Body>
-        </Card>
+        </Card >
+
 
     )
 }
--- front-end/src/components/SnakesPage.jsx
+++ front-end/src/components/SnakesPage.jsx
@@ -1,9 +1,28 @@
 import React, { useState, useEffect } from "react"
 import Snakes from "./Snakes"
 
+
+
 const SnakePage = () => {
+
+    const [state, setState] = useState([]);
+    const url = '/api'
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(url);
+            result
+                .json()
+                .then(result => setState(result.snakes))
+                .catch(err => { console.log("error " + err) })
+        }
+        fetchData();
+    }, []);
+
+
     return (
-        <Snakes />
+        <div className='pic-container'>
+            {state.map((snake, i) => <Snakes data={snake} key={i} />)}
+        </div>
     )
 }
 

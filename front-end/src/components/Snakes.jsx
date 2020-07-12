c88eea750e67dc7851f83e442185473d597e62e2
Juan Galeazzi <jgaleazzi@gmail.com>
2020-07-11 20:53:11 -0500

    cleaned ui for mobile

--- front-end/src/components/Snakes.jsx
+++ front-end/src/components/Snakes.jsx
@@ -1,35 +1,42 @@
-import React, { useState } from "react"
-import { Redirect } from "react-router-dom"
-import { Card, Button } from "react-bootstrap"
-
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+import { Card, Button } from "react-bootstrap";
 
 const Snakes = (props) => {
-    const [redirect, setRedirect] = useState(false);
+  const [redirect, setRedirect] = useState(false);
 
+  return (
+    <>
+      {redirect && <Redirect to="" />}
+      <Card bg="light" style={{ maxWidth: "100%", maxHeight: "100%" }}>
+        <Card.Body>
+          <Card.Title>{props.data.longName}</Card.Title>
+          <Card.Img
+            variant="top"
+            src={props.data.image}
+            style={{ marginBottom: "10px" }}
+          />
+          <Card.Subtitle>({props.data.sciName})</Card.Subtitle>
+          <Card.Text style={{ textAlign: "left" }}>
+            {props.data.description}
+          </Card.Text>
+          <div className="upload-btn-wrapper">
+            {props.hasSlug !== undefined ? (
+              <Button
+                className="btn btn-primary"
+                onClick={() => setRedirect(true)}
+              >
+                {" "}
+                Back{" "}
+              </Button>
+            ) : (
+              <></>
+            )}
+          </div>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
 
-    return (
-        <>
-            {redirect && <Redirect to="" />}
-            < Card bg='light' style={{ width: '30rem' }}>
-                <Card.Body>
-                    <Card.Title>{props.data.longName}</Card.Title>
-                    <Card.Img variant="top" src={props.data.image} style={{ marginBottom: '10px' }} />
-                    <Card.Subtitle>({props.data.sciName})</Card.Subtitle>
-                    <Card.Text style={{ textAlign: 'left' }}>
-                        {props.data.description}
-                    </Card.Text>
-                    <div className='upload-btn-wrapper'>
-                        {props.hasSlug !== undefined ?
-
-                            <Button className='btn btn-primary' onClick={() => setRedirect(true)}> Back </Button>
-
-                            : <></>}
-                    </div>
-                </Card.Body>
-            </Card >
-        </>
-
-    )
-}
-
-export default Snakes
+export default Snakes;
--- front-end/src/components/about.jsx
+++ front-end/src/components/about.jsx
@@ -1,34 +1,45 @@
-import React from 'react'
-import { Button, Card } from 'react-bootstrap'
-import snake from '../assets/snake.png'
+import React from "react";
+import { Button, Card } from "react-bootstrap";
+import snake from "../assets/snake.png";
 const About = () => {
-    return (
-        <div className="pic-container">
-            <Card bg='light' style={{ width: '30rem' }}>
-                <Card.Body>
-                    <Card.Img variant='top' className={'card-img-top'} src={snake} />
-                    <Card.Text></Card.Text>
-                    <Card.Title>
-                        Central Texas A.I. Snake Identifier
-                    </Card.Title>
-                    <Card.Text style={{ textAlign: "left" }}>
-                        Snake id  is a simple machine learning project aiming to identify snakes commonly found in central Texas.<br />
-                        The project was created as part of a Machine Learning group project for the Visualization and Data Analysis course at UT@Austin.<br />
-                        It utilizes the TensorFlow framework and TensorFlow Hub to ingest pre trained images. <br />
-                        For a detailed explanation and tutorial read the <a href='https://www.tensorflow.org/hub/tutorials/image_retraining'> Image retraining tutorial </a> at the TensorFlow website<br />
-                        <div>
-                            Mauricio Gonzalez<br />
-                            Juan L. Galeazzi<br />
-                            David Marcus<br />
-                            Mukit Samad<br />
-                        </div>
+  return (
+    <div className="pic-container">
+      <Card bg="light" style={{ maxWidth: "100%", maxHeight: "100%" }}>
+        <Card.Body>
+          <Card.Img variant="top" className={"card-img-top"} src={snake} />
+          <Card.Text></Card.Text>
+          <Card.Title>Central Texas A.I. Snake Identifier</Card.Title>
+          <Card.Text style={{ textAlign: "left" }}>
+            Snake id is a simple machine learning project aiming to identify
+            snakes commonly found in central Texas.
+            <br />
+            The project was created as part of a Machine Learning group project
+            for the Visualization and Data Analysis course at UT@Austin.
+            <br />
+            It utilizes the TensorFlow framework and TensorFlow Hub to ingest
+            pre trained images. <br />
+            For a detailed explanation and tutorial read the{" "}
+            <a href="https://www.tensorflow.org/hub/tutorials/image_retraining">
+              {" "}
+              Image retraining tutorial{" "}
+            </a>{" "}
+            at the TensorFlow website
+            <br />
+            <div>
+              Mauricio Gonzalez
+              <br />
+              Juan L. Galeazzi
+              <br />
+              David Marcus
+              <br />
+              Mukit Samad
+              <br />
+            </div>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
 
-                    </Card.Text >
-                </Card.Body >
-            </Card >
-        </div >
-    )
-}
-
-
-export default About
+export default About;
